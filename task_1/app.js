9ee96f501e44b580154795ec7aefabb199228109
Swibm <swibm9009@gmalil.com>
2023-01-28 19:16:19 +0600

    files created

---
+++ task_1/app.js
@@ -0,0 +1,33 @@
+﻿const som = document.querySelector("#som")
+const usd = document.querySelector("#usd")
+const won = document.querySelector("#won")
+
+
+
+const convert = (id, target1, target2) => {
+    id.addEventListener("input", () => {
+        const request = new XMLHttpRequest()
+        request.open("GET", "data.json")
+        request.setRequestHeader("Content-Type", "application/json")
+        request.send()
+        request.addEventListener("load", () => {
+            const data = JSON.parse(request.response)
+            
+            if(id === won) {target1.value = (won.value * data.won_som).toFixed(2)
+                    target2.value = (won.value / data.usd_won).toFixed(2)
+                    }
+            else if (id === usd) {target1.value = (usd.value * data.usd_won).toFixed(2)
+                target2.value = (usd.value * data.usd_som).toFixed(2)
+                }
+            else if (id === som) {target1.value = (som.value / data.won_som).toFixed(2)
+                target2.value = (som.value / data.usd_som).toFixed(2)
+            }
+            id.value === "" && (target1.value = "")
+            id.value === "" && ( target2.value = "")
+        })
+    })
+}
+
+convert(won, som, usd)
+convert(usd, won, som)
+convert(som, won, usd)
---
+++ task_2/Menu/js/app.js
@@ -0,0 +1,114 @@
+﻿// 1)сделать слайдер автоматическим и добавить анимации к картинкам 
+
+const tabs = document.querySelectorAll(".tabheader__item")
+const tabsParent = document.querySelector(".tabheader__items")
+const tabsContent = document.querySelectorAll(".tabcontent")
+let counter = 0
+
+const autoSlider = () => {
+    tabsContent.forEach((item) => {
+        item.style.display = "none"
+    })
+    tabs.forEach((item) => {
+        item.classList.remove("tabheader__item_active")
+    })
+    
+    if(counter<=3) {
+        tabsContent[counter].style.display = "block"
+        tabs[counter].classList.add("tabheader__item_active")
+        counter++
+    }
+    else {
+        counter = 0
+        tabsContent[counter].style.display = "block"
+        tabs[counter].classList.add("tabheader__item_active")
+        counter++
+    }
+    setTimeout(autoSlider, 3000)
+}
+
+autoSlider()
+
+//Вызов модалки по нажатию
+
+const modalTrigger = document.querySelector(".btn_white")
+
+const modal = document.querySelector(".modal")
+
+const modalCLoseBtn = document.querySelector(".modal__close")
+
+const openModal = () => {
+    modal.classList.add("show")
+    modal.classList.remove("hide")
+    document.body.style.overflow = "hidden"
+}
+
+const closeModal = () => {
+    modal.classList.add("hide")
+    modal.classList.remove("show")
+    document.body.style.overflow =""
+}
+
+modalTrigger.addEventListener("click", openModal)
+modalCLoseBtn.addEventListener("click", closeModal)
+
+modal.addEventListener("click", (e) => {
+    if(e.target.classList.contains("modal")) {
+        closeModal()
+    }
+})
+
+//dz2 открывать модальное окно как только пользователь будет доскроливать до конца сайта
+
+const openModalEndOfSite = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+        document.body.style.overflow = "hidden";
+        window.scrollBy(0, -300);
+    }
+};
+
+window.addEventListener("scroll", openModalEndOfSite)
+
+const forms = document.querySelectorAll("form")
+
+const postData = (form) => {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        const request = new XMLHttpRequest()
+        request.open("POST", "server.php")
+        request.setRequestHeader("Content-Type", "application/json")
+
+        const formData = new FormData(form)
+        console.log(formData)
+
+        const obj = {}
+
+        formData.forEach((item, name)=> {obj[name] = item})
+
+        const json = JSON.stringify(obj)
+
+        request.send(json)
+        request.addEventListener("load", () => {
+            if(request.status === 200){
+                btnSubmit = document.querySelector(".btn_recall")
+                btnSubmit.style.backgroundColor = "green"
+                btnSubmit.innerHTML = "Успешно!"
+            }
+            else {
+                btnSubmit = document.querySelector(".btn_recall")
+                btnSubmit.style.backgroundColor = "red"
+                btnSubmit.innerHTML = "Произошла ошибка"
+            }
+        })
+
+    })
+}
+
+forms.forEach((item) => {
+    postData(item)
+})
+
+//dz2 выводить сообщение на экран. Если статус 200, то выводить сообщение, что все ок после отправки объекта
